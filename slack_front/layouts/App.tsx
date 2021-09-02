415c078e99f99253f13b162905c90a5c25f35988
JaessJang <jaess@bzznbyd.com>
2021-09-03 00:11:12 +0900

    devserver 설정 진행, hotReload 중요한 설정.

--- slack_front/layouts/App.tsx
+++ slack_front/layouts/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   return(
-    <div>render 입니다.</div>
+    <div>render 입니다 config 설정 할 때 버전 문제로 많은 해결책을 겪었음.</div>
   )
 }
 
--- slack_front/webpack.config.ts
+++ slack_front/webpack.config.ts
@@ -1,70 +1,87 @@
 import path from "path";
-// import from '@pmm';
-import webpack from 'webpack';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+import webpack, { Configuration as WebpackConfiguration } from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin' //타입스크립트 체킹과 웹팩 체킹을 동시에 돌아가도록 해줌.
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
 
 const isDeveloment = process.env.NODE_ENV !== 'production';
-const config: webpack.Configuration = {
-  name : 'sleact',
-  mode : isDeveloment ? 'development' : 'production',
-  devtool : isDeveloment ? 'hidden-source-map' : 'inline-source-map',
-  resolve : {
-    extensions : ['.js', '.jsx', '.ts', '.tsx', '.json'], //babel이 처리할 확장자
-    alias : { // 
-      '@hooks' : path.resolve(__dirname, 'hooks'), // webpack에서 설정 babel에서 체크하기 위해
-      '@components' : path.resolve(__dirname, 'components'),
-      '@layouts' : path.resolve(__dirname, 'layouts'),
-      '@pages' : path.resolve(__dirname, 'pages'),
-      '@utils' : path.resolve(__dirname, 'utils'),
-      '@typings' : path.resolve(__dirname, 'typings'),
+const config: Configuration = {
+  name: 'sleact',
+  mode: isDeveloment ? 'development' : 'production',
+  devtool: isDeveloment ? 'hidden-source-map' : 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], //babel이 처리할 확장자
+    alias: { // 
+      '@hooks': path.resolve(__dirname, 'hooks'), // webpack에서 설정 babel에서 체크하기 위해
+      '@components': path.resolve(__dirname, 'components'),
+      '@layouts': path.resolve(__dirname, 'layouts'),
+      '@pages': path.resolve(__dirname, 'pages'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@typings': path.resolve(__dirname, 'typings'),
     },
   },
-  entry : {
-    app : './client'
+  entry: {
+    app: './client'
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.tsx?$/,
-        loader : 'babel-loader',
-        options : {
-          presets : [
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
             [
               '@babel/preset-env',
               {
-                targets : {browsers : ['last 2 chrome versions']},
-                debug : isDeveloment
+                targets: { browsers: ['IE 10'] },
+                debug: isDeveloment
               },
             ],
             '@babel/preset-react',
             '@babel/preset-typescript',
           ], //모든 소스코드를 ie에서까지 돌아 갈 수 있도록 만들어준다. babel이
-         
+          env: {
+            development: {
+              plugins: [require.resolve('react-refresh/babel')], //hot reloading 에 필요한.
+            },
+          },
         },
-        exclude : path.join(__dirname,'node_modules'),
+        exclude: path.join(__dirname, 'node_modules'),
       },
       {
-        test : /\.css?$/,
+        test: /\.css?$/,
         use: ['style-loader', 'css-loader'], //css 파일을 js로 변환시켜준다. 
       }
     ]
   },
-  plugins : [
-    new ForkTsCheckerWebpackPlugin({
-      async : false
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({ //ts와 webpack 동시에 돌아가도록 함.
+      async: false
     }),
     //process.env.NODE_ENV를 접근 가능하도록 해줌.
-    new webpack.EnvironmentPlugin({NODE_ENV: isDeveloment ? 'development' : 'production'}), 
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDeveloment ? 'development' : 'production' }),
   ],
-  output :{
-    path : path.join(__dirname,'dist'),
-    filename :'[name].js', //entry 에 설정된 값들을 주루룩 만들어줌.
-    publicPath : '/dist/',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js', //entry 에 설정된 값들을 주루룩 만들어줌.
+    publicPath: '/dist/',
   },
-  // devServer : {
-  //   historyApiFallback : true,
-
-  // },
+  devServer: {
+    historyApiFallback: true, //react router 할 때 필요한 설정
+    port: 3090,
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+  },
 };
+
+if (isDeveloment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin()); //hot reload 해주는것들
+  config.plugins.push(new ReactRefreshWebpackPlugin()); //refresh마다 호출 해주는 socket 플러그인.
+}
 
 export default config; //요거 빼먹어서 build 안된거 찾음...

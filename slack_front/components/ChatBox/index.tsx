df9991f364583d3de81fbe0eb82897c43bb54225
JaessJang <jaess@bzznbyd.com>
2021-10-04 15:39:44 +0900

    1. DM,채널 리스트 생성 2. DM 페이지 생성 3. 채팅 전송 박스 생성 4. ref typescript 에서 사용 시 <htmltype>useRef(null) 방법을 통해 예외처리. 5. type없는 npm 설치시에는 항상 typing을 추가로 설치하자.

--- slack_front/components/ChatBox/index.tsx
+++ slack_front/components/ChatBox/index.tsx
@@ -1,21 +1,57 @@
-import React, { useCallback, VFC } from 'react';
+import React, { useCallback, useEffect, useRef, VFC } from 'react';
 import { ChatArea, Form, MentionsTextarea, SendButton, Toolbox } from './styles';
-
+import autosize from 'autosize';
 interface Props {
   chat: string;
+  onSubmitForm: (e: any) => void;
+  onChangeChat: (e: any) => void;
+  placeholder: string;
 }
 
-const ChatBox: VFC<Props> = ({ chat }) => {
-  const onSubmitForm = useCallback(() => {}, []);
+const ChatBox: VFC<Props> = ({ chat, onSubmitForm, onChangeChat, placeholder }) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null); //null과 HTMLTextAreaElement를 추가하여 해결한다.
+  useEffect(() => {
+    if (textareaRef.current) {
+      autosize(textareaRef.current);
+    }
+  }, []);
+
+  const onKeyDownChat = useCallback(
+    (e) => {
+      if (e.key === 'Enter') {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          onSubmitForm(e);
+          console.log('test chatBox');
+        }
+      }
+    },
+    [onSubmitForm],
+  );
 
   return (
     <ChatArea>
       <Form onSubmit={onSubmitForm}>
-        <MentionsTextarea />
+        <MentionsTextarea
+          value={chat}
+          onChange={onChangeChat}
+          onKeyDown={onKeyDownChat}
+          placeholder={placeholder}
+          ref={textareaRef} // null을 useRef에다가 null을 넣어주면 해결됨.
+        />
         <Toolbox>
-          <SendButton></SendButton>
+          <SendButton
+            className={
+              'c-button-unstyled c-icon_button c-icon_button--light c-icon_button--size_medium c-texty_input__button c-texty_input__button--send' +
+              (chat?.trim() ? '' : ' c-texty_input__button--disabled')
+            }
+          >
+            <i className="c-icon c-icon--paperplane-filled" aria-hidden="true" />
+          </SendButton>
         </Toolbox>
       </Form>
     </ChatArea>
   );
 };
+
+export default ChatBox;
--- slack_front/pages/Channel/index.tsx
+++ slack_front/pages/Channel/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+  const onSubmitForm = useCallback((e) => {
+    setChat('');
+  }, []);
+
   return (
     <Container>
       <Header>channel이다!</Header>
+      <ChatList />
+      <ChatBox chat={chat} onSubmitForm={onSubmitForm} onChangeChat={onChangeChat} placeholder="채널을 입력해주세요." />
     </Container>
   );
 };

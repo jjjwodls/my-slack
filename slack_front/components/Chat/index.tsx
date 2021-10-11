3f10d484712c57a8eab23198c1d75030d65c2547
JaessJang <jaess@bzznbyd.com>
2021-10-11 23:37:40 +0900

    멘션기능, 정규표현식 작업 진행 1. 멘션 기능구현 및 정규표현식 문자열 변환 작업

--- slack_front/components/Chat/index.tsx
+++ slack_front/components/Chat/index.tsx
@@ -1,15 +1,44 @@
 import { IDM } from '@typings/db';
-import React, { VFC } from 'react';
+import React, { useMemo, VFC } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+
+  //@[제스1](7)
+  //\d 숫자 +는 1개이상 ?는 0개이상 g는 모두찾기
+  // 예를들어 아이디가 @[제스]12(7) 이면
+  // +? 1개 이상이면서 최대한 적게 찾는다.
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,7 +49,7 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
